27cc0b7f947ae674bed05c5392bae3266880829c
narmatoff <narmatoff@ya.ru>
2025-07-17 15:01:02 +0300

    Update add-wg-routes.sh

--- add-wg-routes.sh
+++ add-wg-routes.sh
@@ -4,16 +4,16 @@ source ./const.sh
 
 echo "# === Добавление маршрутов ==="
 
-for DOMAIN in "${EXCLUDED_DOMAINS[@]}"; do
-  dig +short "$DOMAIN" | grep -E '^[0-9.]+' | sort -u | while read -r IP; do
-    sudo route -n add "$IP/32" "$LOCAL_GATEWAY"
-  done
+for SUBNET in "${STATIC_ROUTES[@]}"; do
+  sudo route -n add "$SUBNET" "$LOCAL_GATEWAY"
 done
 
 for IP in "${EXCLUDED_IPS[@]}"; do
   sudo route -n add "$IP/32" "$LOCAL_GATEWAY"
 done
 
-for SUBNET in "${STATIC_ROUTES[@]}"; do
-  sudo route -n add "$SUBNET" "$LOCAL_GATEWAY"
+for DOMAIN in "${EXCLUDED_DOMAINS[@]}"; do
+  dig +short "$DOMAIN" | grep -E '^[0-9.]+' | sort -u | while read -r IP; do
+    sudo route -n add "$IP/32" "$LOCAL_GATEWAY"
+  done
 done
